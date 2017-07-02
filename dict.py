8f6dcdd40aa26b57646561830c98b2802464c2ea
Jing He <jhe@zendesk.com>
2017-07-02 10:16:57 +0800

    Initial commit

---
+++ dict.py
@@ -0,0 +1,16 @@
+# !/usr/local/bin/python3
+
+# Initiate a nest dictionary
+def init(data):
+    data['first'] = {}
+    data['second'] = {}
+    data['third'] = {}
+
+def lookup(data,label,sub_label)
+    return data[label].get(sub_label)
+
+def store(data,
+
+storage = {}
+init(storage)
+          print storage:
